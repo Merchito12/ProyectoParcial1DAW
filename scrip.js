0348efa6493b67e09c69e135e631c981eff25f93
AliVasped200 <152574439+AndresVasped@users.noreply.github.com>
2024-02-22 13:12:47 -0500

    Elementos logoca etc
    siuuuuuuuuu

--- scrip.js
+++ scrip.js
@@ -1,7 +1,6 @@
 const productos=document.querySelectorAll(".btn");//cojemos la clase
 const pantalla=document.getElementById("buy");
-
-const elementos={};
+const resultado=document.getElementById("result");
 
 for(const producto  of productos)
 {
@@ -9,80 +8,52 @@ for(const producto  of productos)
         
         const precioEm=producto.querySelector('.Precio').innerText;
         const nombreEm=producto.querySelector('.Nombre').innerText;
-    
+
         let imagen=document.createElement("img");//creamos un nuevo elemento tipo imagen
         //y ese elemento imagen sera la imagen del boton
         imagen.src=producto.querySelector('img').src;//obtenemos el src del archivo html
         imagen.alt=producto.querySelector('img').alt;//obtneemos el alt del html
 
-        imagen.style.height='50px';
-        imagen.style.width='50%';
-        imagen.style.borderRadius='0';
-          
+        imagen.style.height='80px';
+        imagen.style.width='auto';
+        imagen.style.borderRadius='20px';
+
+        // Crear elementos para mostrar el nombre y el precio en la sección
+        let precio=document.createElement('p');
+        precio.textContent=precioEm;//obtennemos el valor del precio
+
+        let nombre=document.createElement('p');
+        nombre.textContent=nombreEm;
         
-        if(!(nombreEm in elementos))
-        {
-            elementos[nombreEm]=1;
-            // Crear elementos para mostrar el nombre y el precio en la sección
-            let precio=document.createElement('p');
-            precio.textContent=precioEm;//obtennemos el valor del precio
+        let elimina=document.createElement('button');
+        //agregamos a mi section los elementos
+        
+        elimina.style.height='20px';
+        elimina.style.width='50px';
+        elimina.textContent='delete';
 
-            let nombre=document.createElement('p');
-            nombre.textContent=nombreEm;
+        pantalla.appendChild(elimina);
+        pantalla.appendChild(imagen);
+        pantalla.appendChild(nombre);
+        pantalla.appendChild(precio);
+        
+        const preci=precioEm.replace('$', '');
+        let total=parseFloat(resultado.innerText);//obtenemos la info de resultado
+        total+=parseFloat(preci);//sumamos
+        resultado.textContent=total.toFixed(2);//agregamos dos decimales
 
-            let añade=document.createElement('li');
-            añade.textContent = `${nombreEm}: 1`;
-            
-            let elimina=document.createElement('button');
-            //agregamos a mi section los elementos
-            
+        
+        elimina.addEventListener('click',()=>{
+            pantalla.removeChild(imagen);
+            pantalla.removeChild(nombre);
+            pantalla.removeChild(precio);
+            pantalla.removeChild(elimina);
 
-            
-            pantalla.appendChild(elimina);
-            pantalla.appendChild(imagen);
-            //pantalla.appendChild(nombre);
-            pantalla.appendChild(precio);
-            pantalla.appendChild(añade);
-            
-            
-
-            elimina.addEventListener('click',()=>{
-
-                if(contador===0)
-                {
-                    pantalla.removeChild(imagen);
-                    pantalla.removeChild(nombre);
-                    pantalla.removeChild(precio);
-                    pantalla.removeChild(elimina);
-                }
-                else
-                {
-                    contador--;
-                }
-                        
-            })
-        }
-        else
-        {
-            elementos[nombreEm]++;
-            // Si el menú ya existe en la lista, buscar el elemento existente y actualizar su contenido
-            let listaElementos = pantalla.getElementsByTagName('li');
-            let elementoExistente;
-
-            for (let i = 0; i < listaElementos.length; i++) 
-            {
-                if (listaElementos[i].textContent.includes(nombreEm)) {
-                    elementoExistente = listaElementos[i];
-                    break;
-                }
-            }
-            if (elementoExistente) {
-                let cantidad = elementos[nombreEm];
-                elementoExistente.textContent = `${nombreEm}: ${cantidad}`;
-            }
-        }
-
-         
+            let resta=parseFloat(resultado.innerText);
+            resta-=parseFloat(preci);
+            resultado.textContent=resta.toFixed(2);    
+        })
+        
     })
 }
  
